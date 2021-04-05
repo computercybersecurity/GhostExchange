0948cde12976bd9867ac536d0fd2cdbf599e45ba
yakitori0x <yakitori0x@protonmail.com>
2021-04-05 15:47:05 +0900

    fix migrator test

--- migrations/4_deploy_timelock.js
+++ migrations/4_deploy_timelock.js
@@ -2,7 +2,7 @@ const TimeLock = artifacts.require("TimeLock");
 const KingGhost = artifacts.require("KingGhost");
 
 module.exports = async function (deployer) {
-  const admin = "";
+  const admin = "0xcE637E04AD717d8899525808eE50c0276c115607";
   const delay = "172800";
   const kingGhost = await KingGhost.deployed();
   await deployer.deploy(TimeLock, admin, delay);
--- test/Migrator.test.js
+++ test/Migrator.test.js
@@ -4,8 +4,8 @@ const KingGhost = artifacts.require("KingGhost");
 const MockERC20 = artifacts.require("MockERC20");
 const GhostswapPair = artifacts.require("GhostswapPair");
 const GhostswapFactory = artifacts.require("GhostswapFactory");
-const GhostswapRouter = artifacts.require("GhostswapRouter02");
-const Migrator = artifacts.require("Migrator2");
+const GhostswapRouter = artifacts.require("MockGhostswapRouter02");
+const Migrator = artifacts.require("Migrator");
 
 contract("Migrator", ([alice, bob, dev, minter]) => {
   beforeEach(async () => {
@@ -16,11 +16,11 @@ contract("Migrator", ([alice, bob, dev, minter]) => {
       from: minter,
     });
     this.router1 = await GhostswapRouter.new(
-      this.factor1.address,
+      this.factory1.address,
       this.wbnb.address
     );
     this.router2 = await GhostswapRouter.new(
-      this.factor2.address,
+      this.factory2.address,
       this.wbnb.address
     );
     this.token = await MockERC20.new("TOKEN", "TOKEN", "100000000", {
@@ -49,6 +49,7 @@ contract("Migrator", ([alice, bob, dev, minter]) => {
     );
     await this.ghost.transferOwnership(this.king.address, { from: alice });
     await this.king.add("100", this.lp1.address, { from: alice });
+    await this.king.add("100", this.lp2.address, { from: alice });
   });
 
   it("should do the migration successfully", async () => {
@@ -56,45 +57,23 @@ contract("Migrator", ([alice, bob, dev, minter]) => {
     await this.wbnb.transfer(this.lp1.address, "500000", { from: minter });
     await this.lp1.mint(minter);
     assert.equal((await this.lp1.balanceOf(minter)).valueOf(), "2235067");
-    // Add some fake revenue
-    await this.token.transfer(this.lp1.address, "100000", { from: minter });
-    await this.wbnb.transfer(this.lp1.address, "5000", { from: minter });
-    await this.lp1.sync();
     await this.lp1.approve(this.king.address, "100000000000", { from: minter });
     await this.king.deposit("0", "2000000", { from: minter });
     assert.equal(
       (await this.lp1.balanceOf(this.king.address)).valueOf(),
       "2000000"
     );
-    await this.king.migrate(0);
+    await this.king.setMigrator(this.migrator.address, { from: alice });
+    await this.king.migrate(0, 1, { from: minter });
     assert.equal((await this.lp1.balanceOf(this.king.address)).valueOf(), "0");
     assert.equal(
       (await this.lp2.balanceOf(this.king.address)).valueOf(),
-      "2000000"
+      "1998999"
     );
-    await this.king.withdraw("0", "2000000", { from: minter });
-    await this.lp2.transfer(this.lp2.address, "2000000", { from: minter });
+    await this.king.withdraw("1", "1998999", { from: minter });
+    await this.lp2.transfer(this.lp2.address, "1998999", { from: minter });
     await this.lp2.burn(bob);
-    assert.equal((await this.token.balanceOf(bob)).valueOf(), "9033718");
-    assert.equal((await this.wbnb.balanceOf(bob)).valueOf(), "451685");
-  });
-
-  it("should allow first minting from public only after migrator is gone", async () => {
-    await this.factory2.setMigrator(this.migrator.address, { from: alice });
-    this.tokenx = await MockERC20.new("TOKENX", "TOKENX", "100000000", {
-      from: minter,
-    });
-    this.lpx = await GhostswapPair.at(
-      (await this.factory2.createPair(this.wbnb.address, this.tokenx.address))
-        .logs[0].args.pair
-    );
-    await this.wbnb.transfer(this.lpx.address, "10000000", { from: minter });
-    await this.tokenx.transfer(this.lpx.address, "500000", { from: minter });
-    await expectRevert(this.lpx.mint(minter), "Must not have migrator");
-    await this.factory2.setMigrator(
-      "0x0000000000000000000000000000000000000000",
-      { from: alice }
-    );
-    await this.lpx.mint(minter);
+    assert.equal((await this.token.balanceOf(bob)).valueOf(), "8939802");
+    assert.equal((await this.wbnb.balanceOf(bob)).valueOf(), "446989");
   });
 });
